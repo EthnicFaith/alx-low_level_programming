e2a986ba47c8232168d4f7cd17ac8ea3c5adf906
EthnicFaith <faith.s@ethnicroyals.co.za>
2023-06-15 16:55:01 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio. h>
+/**
+ * main - A program that prints exactly with proper grammar, but the outcome is a piece of art
+ * Return: 0 (Success)
+ * int main(void)
+{
+        printf("program that prints exactly with proper grammar, but the outcome is a piece of art,\n")
+	return(0);
+}	
